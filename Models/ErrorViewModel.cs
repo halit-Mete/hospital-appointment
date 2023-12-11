4c929de64b2b8625a02bcd562648f9dba8ea60d0
halit-Mete <104191379+halit-Mete@users.noreply.github.com>
2023-12-11 18:06:01 +0300

    models

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace İdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
