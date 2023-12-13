324297828d64907ef1204205e9dd9d133571b814
halit-Mete <104191379+halit-Mete@users.noreply.github.com>
2023-12-13 18:14:19 +0300

    Controllers

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,48 @@
+﻿using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        public RolesController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_roleManager.Roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AppRole model)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                var result = await _roleManager.CreateAsync(model);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach(var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
+            // valid değilse modeli tekrar sayfaa üzerine yansıtıyoruz
+            return View(model);
+        }
+    }
+}
