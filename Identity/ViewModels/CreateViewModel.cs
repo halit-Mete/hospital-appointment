729b0becc098156d16c6c6e1f221c1840388467f
halit-Mete <104191379+halit-Mete@users.noreply.github.com>
2023-12-17 18:08:45 +0300

    system

---
+++ Identity/ViewModels/CreateViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.ViewModels
+{
+	public class CreateViewModel
+	{
+        [Display(Name = "Kullanıcı Adı")]
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        [Display(Name = "Tam Adı")]
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+		[EmailAddress]
+		public string Email { get; set; } = string.Empty;
+
+        [Display(Name = "Şifre")]
+        [Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; } = string.Empty;
+
+        [Display(Name = "Şifre Tekrar")]
+        [Required]
+		[DataType(DataType.Password)]
+		[Compare(nameof(Password), ErrorMessage = "Parola Eşleşmiyor")] // password ile karşılaştırıp aynı mı karşılaştırıyor
+		public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
