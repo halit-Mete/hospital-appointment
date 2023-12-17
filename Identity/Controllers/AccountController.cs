ebd340425d4c50b92a184f9ea474a0ad4dbb7882
halit-Mete <104191379+halit-Mete@users.noreply.github.com>
2023-12-17 14:43:58 +0300

    system

---
+++ Identity/Controllers/AccountController.cs
@@ -0,0 +1,78 @@
+﻿using Identity.Models;
+using Identity.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private UserManager<AppUser> _userManager;
+
+        private RoleManager<AppRole> _roleManager;
+
+        private SignInManager<AppUser> _signInManager;
+        public AccountController(
+            UserManager<AppUser> userManager,
+            RoleManager<AppRole> roleManager,
+            SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if(user != null)
+                {
+                    await _signInManager.SignOutAsync();
+
+                    var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, true);
+
+                    if(result.Succeeded) // başarılı ise başarısızlıktaki kilitlenme olaylarını resetliyoruz
+                    {
+                        await _userManager.ResetAccessFailedCountAsync(user);
+                        await _userManager.SetLockoutEndDateAsync(user, null); // null diyerek hemen sıfırlanmasını sağlıyoruz
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else if(result.IsLockedOut) // kullanıcı girişi başarısızsa
+                    {
+                        var lockoutDate = await _userManager.GetLockoutEndDateAsync(user);
+                        var timeLeft = lockoutDate.Value - DateTime.UtcNow;
+                        ModelState.AddModelError("", $"Hesabınız kilitlendi. Lütfen {timeLeft.Minutes} dakika {timeLeft.Seconds} saniye sonra tekrar deneyiniz.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Hatalı parola");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Bu email adresiyle bir hesap bulunamadı");
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout() // coockie yi silerek logout yaptırıyor
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login");
+        }
+     
+    }
+}
---
+++ Identity/Models/IdentitySeedData.cs
@@ -0,0 +1,47 @@
+﻿using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Models
+{
+    public class IdentitySeedData
+    {
+        private const string adminUser = "admin";
+
+        private const string adminPassword = "b211210064";
+
+        public static async void IdentityTestUser(IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<IdentityContext>();
+
+            // Bekleyen migration var mı / Varsa database update e gerek yok olanı aktarabiliriz
+            if (context.Database.GetAppliedMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            // Identity context üzerinden tool sınıfları var onlardan biri userManager hazır servis
+            var userManager = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+
+            // Kullanıcı oluşturma, adminUser'a göre arayacak varsa bize identity user gelecek
+            var user = await userManager.FindByNameAsync(adminUser);
+
+            // Eğer bir user yoksa user oluştursun
+            if (user == null)
+            {
+                user = new AppUser
+                {
+                    FullName = "Halit Mete Tunç",
+                    UserName = adminUser,
+                    Email = "b211210064@sakarya.edu.tr",
+                    PhoneNumber = "5545"
+                };
+                // Kullanıcıyı oluştururken parola atıyor
+                await userManager.CreateAsync(user, adminPassword);
+                // sonradan passwordu verme nedenimiz hash'lenerek veritabanında saklanmasıdır (güvenlik için)
+                // hackleyen kişi bu paraloyı göremez. Bu parolayı hash'leyen algoraitma parolayı valid edebilir
+            }
+        }
+
+    }
+}
