1d5450265ed8b4c96b5d855bbd040ecfba4abc2c
halit-Mete <104191379+halit-Mete@users.noreply.github.com>
2023-12-18 10:18:00 +0300

    system

---
+++ Identity/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Models
+{
+    // Ekstra kullanıcı bilgisi alma
+    public class AppUser: IdentityUser
+    {
+        public string? FullName { get; set; }
+
+    }
+}
---
+++ Identity/Models/Randevu.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models
+{
+    public class Randevu
+    {
+        public int RandevuId { get; set; }
+
+        [Display(Name = "Hasta Adı")]
+        public string? HastaAd { get; set; }
+
+        [Display(Name = "Hasta TC")]
+        public string? HastaTc { get; set; }
+
+        [Display(Name = "Hasta Telefon")]
+        public string? HastaTel { get; set; }
+
+        [Display(Name = "Randevu Tarihi")]
+
+        public int DoktorId { get; set; }
+        public DateTime RandevuTarih { get; set; }
+        public Doktor? Doktor { get; set; }
+    }
+}
