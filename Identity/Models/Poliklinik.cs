5e3b6f81ea446b03e36a3ead159ab1574330aaaa
halit-Mete <104191379+halit-Mete@users.noreply.github.com>
2023-12-18 18:57:26 +0300

    system

---
+++ Identity/Models/Poliklinik.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models
+{
+    public class Poliklinik
+    {
+        [Key]
+        public int PoliklinikId { get; set; }
+
+        [Display(Name = "Poliklinik Adı")]
+        public string? PoliklinikAd { get; set; }
+
+        public List<Doktor> Doktorlar { get; set; } = new List<Doktor>();
+    }
+}
---
+++ Identity/Program.cs
@@ -0,0 +1,92 @@
+using Identity.Models;
+using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Identity islemleri (1)
+builder.Services.AddDbContext<IdentityContext>(options => options.UseSqlite(builder.Configuration["ConnectionStrings:SQLite_Connection"]));
+
+//Randevu Context
+
+builder.Services.AddDbContext<RandevuContext>(options => options.UseSqlite(builder.Configuration["ConnectionStrings:Randevu_Connection"]));
+
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<IdentityContext>();
+// builder.Services.AddIdentity<IdentityUser, IdentityRole> yerine Appleri yazdýk 
+
+// dil desteði kýsmý
+builder.Services.AddLocalization(opt =>
+{
+    opt.ResourcesPath = "Resources";
+});
+
+// dik desteði kýsmý
+builder.Services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();
+
+// Sýfre kurallarý (3)
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Sifre ayarlarý
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+
+    options.User.RequireUniqueEmail = true;
+
+    // kullanýcý ayarlarý
+    options.User.AllowedUserNameCharacters ="abcçdefgðhýijklmnoöpqrsþtuvwxyz_-.";
+
+    // hesap kilitlenme ayarlarý
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/Login"; // Eriþemediðin bir sayfada nereye yönlendirilsin
+    options.SlidingExpiration = true; // 7. günde tekrar girerse 7 gün tekrar set edilir
+    options.ExpireTimeSpan = TimeSpan.FromDays(7);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+// dil desteði kýsmý
+var supportedCultures = new[] { "en", "tr" };
+var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0]).AddSupportedCultures(supportedCultures).AddSupportedCultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// (2)
+IdentitySeedData.IdentityTestUser(app);
+
+app.Run();
